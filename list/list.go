975b86a21478b06fc098efed2c2b10fc71f84e32
wangshu24 <trandinhphuc1997@gmail.com>
2025-03-21 12:52:03 -0400

    setup main go, moved lru to its own package, for ease of testing via main, rename attributes to be exportable for list.Entry[K,V]

--- list/list.go
+++ list/list.go
@@ -6,19 +6,19 @@ import (
 )
 
 type Entry[K comparable, V any] struct {
-	next, back *Entry[K, V]
-	key K
-	value V
-	bday     time.Time
-	ttl time.Duration
+	Next, Back *Entry[K, V]
+	Key        K
+	Value      V
+	Bday       time.Time
+	TTL        time.Duration
 }
 
 type List[K comparable, V any] struct {
-	tail, root Entry[K, V]
-	len, cap      int
+	Tail, Root Entry[K, V]
+	len, cap   int
 }
 
-type V interface {}
+type V interface{}
 
 func NewList[K comparable, V any](cap int) *List[K, V] {
 	l := new(List[K, V])
@@ -26,47 +26,48 @@ func NewList[K comparable, V any](cap int) *List[K, V] {
 	if cap > 0 {
 		c = cap
 	}
-	l.cap=  c
-	l.root.next = &l.root
-	l.tail.back = &l.root 
+	l.cap = c
+	l.Root.Next = &l.Root
+	l.Tail.Back = &l.Root
 	return l
 }
 
-//Add add a new item to the list and return boolean for whether an old item was discarded or not
-//TODO: added condition to check for existing key value pair, if exist, move to top of list and remove old one 
-func (l *List[K , V]) Add(e Entry[K, V])  bool {
+// Add add a new item to the list and return boolean for whether an old item was discarded or not
+// TODO: added condition to check for existing key value pair, if exist, move to top of list and remove old one
+func (l *List[K, V]) Add(e Entry[K, V]) bool {
 	if l.len == 0 {
-		l.root = e
-		l.tail = e
+		l.Root = e
+		l.Tail = e
+		l.len++
 		return false
 	}
 
-	l.tail.next =  &e
-	l.tail = e
+	l.Tail.Next = &e
+	l.Tail = e
 	l.len++
 	evicted := l.len > l.cap
 	if l.len > l.cap {
-		l.root = *l.root.next
+		l.Root = *l.Root.Next
 		l.len--
 	}
- 	return evicted
+	return evicted
 }
 
-func (l *List[K, V]) GetInd(index int) ( *Entry[K,V] , error) {
-	if index > l.len || index > l.cap { 
+func (l *List[K, V]) GetInd(index int) (*Entry[K, V], error) {
+	if index > l.len || index > l.cap {
 		return nil, errors.New("invalid index")
 	}
 
-	tmp := l.root
-	for i:=0; i < index;i++{
-		tmp = *tmp.next
+	tmp := l.Root
+	for i := 0; i < index; i++ {
+		tmp = *tmp.Next
 	}
 	time := time.Now()
-	endtime := tmp.bday.Add(tmp.ttl)
-	if (time.After(endtime)) {
+	endtime := tmp.Bday.Add(tmp.TTL)
+	if time.After(endtime) {
 		return nil, errors.New("stale cache")
 	}
-	tmp.bday = time
+	tmp.Bday = time
 	return &tmp, nil
 }
 
@@ -74,29 +75,29 @@ func (l *List[K, V]) RemoveInd(index int) error {
 	if index > l.len || index > l.cap {
 		return errors.New("invalid index")
 	}
-	tmp := l.root 
-	for i:=0; i < index; i++ {
-		tmp = *tmp.next
+	tmp := l.Root
+	for i := 0; i < index; i++ {
+		tmp = *tmp.Next
 	}
 
-	back := tmp.back
-	next := tmp.next
-	back.next = next
+	back := tmp.Back
+	next := tmp.Next
+	back.Next = next
 	return nil
 }
 
-func (l *List[K, V]) PeekInd(index int) (*Entry[K, V], error){ 
+func (l *List[K, V]) PeekInd(index int) (*Entry[K, V], error) {
 	if index > l.cap || index > l.len {
 		return nil, errors.New("invalid index")
 	}
 
-	tmp := l.root
-	for i:=0; i < index; i++ {
-		tmp = *tmp.next
+	tmp := l.Root
+	for i := 0; i < index; i++ {
+		tmp = *tmp.Next
 	}
 
 	return &tmp, nil
-} 
+}
 
 func (l *List[K, V]) Len() int {
 	return l.len
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
-package lru
+package main
+
+import (
+	"fmt"
+	"lrucache_experiment/list"
+	"time"
+)
 
 func main() {
+	newList := list.NewList[string, string](10)
+	fmt.Println(*newList)
 
+	entry1 := &list.Entry[string, string]{
+		Next:  nil,
+		Back:  nil,
+		Key:   "key",
+		Value: "value",
+		Bday:  time.Now(),
+		TTL:   time.Duration(5 * 10000000000),
+	}
+	newList.Add(*entry1)
+	fmt.Println(*newList)
+	fmt.Println(newList.Len())
 }
